c1579e0a8fc39ad80e69da9ebc43a94b46d33b35
acer <acer@LAPTOP-H9QES7A9>
2024-01-28 18:03:28 +0100

    Aggiungere i file di progetto.

---
+++ CerchioParse/Cerchio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CerchioParse
+{
+    internal class Cerchio
+    {
+        public float Raggio { get; set; }
+
+        public Cerchio(float raggio) 
+        {
+            Raggio = raggio;
+        }
+
+        public float Area()
+        {
+            return (float)(Math.PI * (Raggio*Raggio));
+        }
+        public float Perimetro()
+        {
+            return (float)(Math.PI * (2 * Raggio));
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}", Raggio);
+        }
+        public static Cerchio Parse(string s)
+        {
+            string[] raggio = s.Split(new char[] { });
+            int Raggio = int.Parse(raggio[0]);
+            return new Cerchio(Raggio);
+        }
+    }
+}
---
+++ CerchioParse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CerchioParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            Cerchio c = Cerchio.Parse(a);
+            Console.WriteLine(c.Area());
+            Console.WriteLine(c.Perimetro());
+
+            Console.ReadLine();
+        }
+    }
+}
